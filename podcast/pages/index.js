9c3dee28be592e5d9ffd7e39785e49750007d734
Michael <memiller219@gmail.com>
2025-07-31 12:04:00 -0700

    Initial Commit & Storyblok connection

---
+++ podcast/pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from "@storyblok/react";
+
+export default function Home({ story }) {
+  story = useStoryblokState(story);
+
+  return (
+    <div>
+      <StoryblokComponent blok={story.content} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  let slug = "home";
+
+  let sbParams = {
+    version: "draft", // or 'published'
+  };
+
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+    },
+    revalidate: 3600,
+  };
+}
